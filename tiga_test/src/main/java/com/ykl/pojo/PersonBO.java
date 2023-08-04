777fe0a68d4c3768a229c8a267dcf5e0afee2d3f
langlang-ye <langlang_sir@163.com>
2023-08-04 22:35:33 +0800

    -- tiga_test: 补交遗漏的文件

---
+++ tiga_test/src/main/java/com/ykl/pojo/PersonBO.java
@@ -0,0 +1,62 @@
+package com.ykl.pojo;
+
+public class PersonBO {
+
+    private Integer id;
+    private String name;
+    private int age;
+    private String remark;
+
+    private Integer page;
+    private Integer pageSize;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+
+}
